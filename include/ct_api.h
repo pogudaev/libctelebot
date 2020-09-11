29e846457e654df00ad85d6b1fcc20b02c679fb7
Alexander Pogudaev <apogudaev@dlink.ru>
2020-09-11 17:24:38 +0300

    add doxygen comment for ct_api and ct_buffer

--- include/ct_api.h
+++ include/ct_api.h
@@ -27,18 +27,69 @@ freely, subject to the following restrictions:
 struct ct_api_s;
 typedef struct ct_api_s ct_api_t;
 
+/**
+ * @brief ct_api_create создает объект ct_api_t
+ * @return указатель на объект ct_api_t, или NULL в случае ошибки
+ * @note объект ct_api_t создается в куче. Требуется очистка памяти функцией ct_api_free
+ */
 ct_api_t *ct_api_create(void);
+
+/**
+ * @brief ct_api_free очищает память выделенную под объект ct_api_t
+ * @param ct_api указатель на объект ct_api_t для которого требуется очистка памяти
+ */
 void ct_api_free(ct_api_t *ct_api);
 
+/**
+ * @brief ct_api_set_token устанавливает токен бота
+ * @param ct_api указатель на объект ct_api_t
+ * @param token указатель на строку с токеном
+ * @note token следует получить у @BotFather
+ */
 void ct_api_set_token(ct_api_t *ct_api, const char *token);
 
+/**
+ * @brief ct_api_update проверяет новые входящие сообщения и загружает их. Для получения загруженного сообщения надо использовать ct_api_recv_message
+ * @param ct_api указатель на объект ct_api_t
+ * @return true - успех, false - ошибка
+ */
 bool ct_api_update(ct_api_t *ct_api);
 
+/**
+ * @brief ct_api_send_text посылает текстовое сообщение в указанный чат
+ * @param ct_api указатель на объект ct_api_t
+ * @param chat_id идентификатор чата
+ * @param text отправляемый текст
+ * @return true - успех, false - ошибка
+ */
 bool ct_api_send_text(ct_api_t *ct_api, ssize_t chat_id, const char *text);
 
+/**
+ * @brief ct_api_get_file загружает файл по указанному идентификатору
+ * @param ct_api указатель на объект ct_api_t
+ * @param file_id идентификатор файла
+ * @return указатель на буффер с файлом
+ * @note буффер ct_buffer_t создается в куче. Требуется очистка памяти функцией ct_buffer_free
+ */
 ct_buffer_t *ct_api_get_file(ct_api_t *ct_api, const char *file_id);
+
+/**
+ * @brief ct_api_send_photo посылает сообщение с картинкой
+ * @param ct_api указатель на объект ct_api_t
+ * @param chat_id идентификатор чата
+ * @param content указатель на буффер с передаваемыми данными
+ * @note буфер должен быть создан в куче, он переходит во владение этой функции и не требует очистки
+ * @note в буфере должно находиться валидное содержимое JPEG-файла, другие форматы пока не поддерживаются
+ * @return true - успех, false - ошибка
+ */
 bool ct_api_send_photo(ct_api_t *ct_api, ssize_t chat_id, ct_buffer_t *content);
 
+/**
+ * @brief ct_api_recv_message возвращает загруженное принятое сообщение
+ * @param ct_api указатель на объект ct_api_t
+ * @return указатель на сообщение типа ct_message_t
+ * @note сообщение ct_message_t создается в куче. Требуется очистка памяти функцией ct_message_free
+ */
 ct_message_t *ct_api_recv_message(ct_api_t *ct_api);
 
 #endif // CT_API_H
--- include/ct_buffer.h
+++ include/ct_buffer.h
@@ -26,18 +26,74 @@ freely, subject to the following restrictions:
 struct ct_buffer_s;
 typedef struct ct_buffer_s ct_buffer_t;
 
+/**
+ * @brief ct_buffer_create создает буфер ct_buffer_t
+ * @return указатель на объект ct_buffer_t, или NULL в случае ошибки
+ * @note объект ct_buffer_t создается в куче. Требуется очистка памяти функцией ct_buffer_free
+ */
 ct_buffer_t *ct_buffer_create(void);
+
+/**
+ * @brief ct_buffer_free очищает память выделенную под объект ct_buffer_t
+ * @param buffer указатель на объект ct_buffer_t для которого требуется очистка памяти
+ */
 void ct_buffer_free(ct_buffer_t *buffer);
 
+/**
+ * @brief ct_buffer_set устанавливает содержимое буфера. Прежние данные удаляются.
+ * @param buffer указатель на объект ct_buffer_t
+ * @param data указатель на область памяти из которой будут скопированы данные в буфер
+ * @param size размер данных которые будут скопированны в буффер
+ */
 void ct_buffer_set(ct_buffer_t *buffer, const void *data, size_t size);
+
+/**
+ * @brief ct_buffer_add добавляет новые данные к тем что уже есть в буффере.
+ * @param buffer указатель на объект ct_buffer_t
+ * @param data указатель на область памяти из которой будут скопированы данные в буфер
+ * @param size размер данных которые будут добавлены в буффер
+ */
 void ct_buffer_add(ct_buffer_t *buffer, const void *data, size_t size);
+
+/**
+ * @brief ct_buffer_get получает данные из буфера копированием в новую область памяти
+ * @param buffer указатель на объект ct_buffer_t
+ * @param data двойной указатель на новую область памяти куда будут записаны данные.
+ * @param size указатель на переменную куда будет записано сколько байт было скопировано
+ * @note для data выделяется память в куче, после использования ее необходимо освободить функцией free
+ */
 void ct_buffer_get(const ct_buffer_t *buffer, void **data, size_t *size);
 
+/**
+ * @brief ct_buffer_get_data возвращает указатель на данные в буфере
+ * @param buffer указатель на объект ct_buffer_t
+ * @return указатель на данные в буфере
+ * @note если буффер пуст будет возвращен валидный указатель
+ */
 const void *ct_buffer_get_data(const ct_buffer_t *buffer);
+
+/**
+ * @brief ct_buffer_get_size возвращает текущий размер буфера
+ * @param buffer указатель на объект ct_buffer_t
+ * @return размер
+ */
 size_t ct_buffer_get_size(const ct_buffer_t *buffer);
 
-#define ct_buffer_get_string(b) (const char *)(ct_buffer_get_data(buff));
+/**
+ * @brief ct_buffer_set_string устанавливает содержимое буфера из строки
+ * @param buffer указатель на объект ct_buffer_t
+ * @param string строка
+ */
 void ct_buffer_set_string(ct_buffer_t *buffer, const char *string);
+
+/**
+ * @brief ct_buffer_get_data возвращает указатель на строку в буфере
+ * @param buffer указатель на объект ct_buffer_t
+ * @return указатель на строку в буфере
+ * @note если буффер пуст будет возвращен указатель на пустую строку
+ */
+#define ct_buffer_get_string(b) (const char *)(ct_buffer_get_data(buff));
+
 
 
 #endif // CT_BUFFER_H
